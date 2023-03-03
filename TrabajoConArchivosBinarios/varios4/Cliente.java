cdc39877cc99c529bf56dd75c4e8e42d1ddd6dc0
jpovvacor96 <jpovvacor96@gmail.com>
2023-03-03 12:31:18 +0100

    Nuevas modificaciones

--- TrabajoConArchivosBinarios/varios4/Cliente.java
+++ TrabajoConArchivosBinarios/varios4/Cliente.java
@@ -2,6 +2,7 @@ package varios4;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cliente {
 	
@@ -12,6 +13,10 @@ public class Cliente {
 	private int id;
 	private String nombre;
 	private int telefono;
+	public static List<Cliente> listaClientes=new ArrayList<>();
+	public static final String RUTA_ARCHIVO="/media/jose/Archivos/DAM/"
+			+ "Programación/eclipse-workspace-1dam/GrupoNTTCantillanaDAM"
+			+ "/TrabajoConArchivosBinarios/varios4/clientes.bin";
 	
 	
 	//#########################
@@ -41,6 +46,43 @@ public class Cliente {
 	public void setTelefono(int n) {
 		this.telefono=n;
 	}
+	
+	//#############
+	//CONSTRUCTORES
+	//#############
+	
+	public Cliente(int id, String nombre, int telefono) {
+		this.setId(id);
+		this.setNombre(nombre);
+		this.setTelefono(telefono);
+	}
+	
+	//###################
+	//MÉTODOS FUNCIONALES
+	//###################
+	
+	public static void nuevoCliente() {
+		int id, telefono;
+		String nombre;
+		System.out.println("Introduzca el identificador del cliente:");
+		id=new Scanner(System.in).nextInt();
+		System.out.println("Introduzca el nombre y los apellidos:");
+		nombre=new Scanner(System.in).nextLine();
+		System.out.println("Introduzca el teléfono:");
+		telefono=new Scanner(System.in).nextInt();
+		new Cliente(id, nombre, telefono);
+		System.out.println("Nuevo cliente añadido correctamente");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
